6178d1442fbac15ace6d5f66910c8ba4d834cb2c
munendra <javadev.munendra@gmail.com>
2023-12-13 15:27:43 +0530

    meal api handling and print the data into front end

---
+++ meal-api/src/Main.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { useState,useEffect } from 'react'
+import './style.css';
+
+const Main = () => {
+    const [items,setItems] = useState([]);
+
+    useEffect(()=>{
+        axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood").then(res=>{
+            setItems(res.data.meals)
+        }).catch(
+            (err)=>{
+                console.log(err);
+            });
+
+        },[])
+
+  const itemsList = items.map(({strMeal,strMealThumb,idMeal})=>{
+    return(
+        <section className='card' key={idMeal}>
+            <img src={strMealThumb} alt={idMeal} />
+            <section className='content'>
+                <p>{strMeal}</p>
+                <p>{idMeal}</p>
+            </section>
+        </section>
+    )
+  })
+  return <div className="items-container">{itemsList}</div>
+}
+
+export default Main
